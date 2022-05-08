225d87c2104a9f3344f96960f205f220bce4618a
Julie <julie.gomez75@gmail.com>
2022-05-08 22:11:48 +0200

    PL-2 [ADD] Create : Create Task Activity.

--- app/src/main/java/com/example/perfectlist/MainActivity.java
+++ app/src/main/java/com/example/perfectlist/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.perfectlist;
 
+import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent_createTaskActivity = new Intent(this, CreateTaskActivity.class);
+        startActivity(intent_createTaskActivity);
     }
 }
